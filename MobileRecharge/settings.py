c52f221da05c026d447ce875833525b7170f0b74
Sakib Malik <maliksakib347@gmail.com>
2022-03-23 22:08:05 +0530

    deplot

--- MobileRecharge/settings.py
+++ MobileRecharge/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,12 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-##y*%*tb2a#xdg9%38)*@54#uh&+z6d*b*gy!uj^ikrzb0txxz'
-
+# SECRET_KEY = 'django-insecure-##y*%*tb2a#xdg9%38)*@54#uh&+z6d*b*gy!uj^ikrzb0txxz'
+SECRET_KEY=os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost","mobileplansakib.herokuapp.com"]
 
 # Application definition
 
@@ -115,7 +115,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: waitress-serve --port=$PORT MobileRecharge.wsgi:application
